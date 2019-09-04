76ff1a02d59e1cf6e2df4bae1fcf0b0974b9ccb7
Johannes Wilm <mail@johanneswilm.org>
2019-09-05 01:02:58 +0200

    3.7.0rc1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='fiduswriter-phplist',
-    version='3.7.0-dev',
+    version='3.7.0rc1',
     packages=find_namespace_packages(),
     include_package_data=True,
     license='AGPL License',
