7f551c1a77695a27a5cd813dee6e5bfd6e10cfb9
Johannes Wilm <mail@johanneswilm.org>
2020-10-24 18:52:13 +0200

    3.9.0-dev

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='fiduswriter-phplist',
-    version='3.8.0',
+    version='3.9.0-dev',
     packages=find_namespace_packages(),
     include_package_data=True,
     exclude_package_data={
@@ -46,12 +46,12 @@ setup(
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
-        'Framework :: Django :: 2.2',
+        'Framework :: Django :: 3.1',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU Affero General Public License v3',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
