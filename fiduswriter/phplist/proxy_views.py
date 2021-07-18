f60f21728a31f338652fc3dbe2eca9eea35abad6
Johannes Wilm <mail@johanneswilm.org>
2021-07-18 14:07:42 +0200

    black

--- fiduswriter/phplist/proxy_views.py
+++ fiduswriter/phplist/proxy_views.py
@@ -9,34 +9,27 @@ from urllib.parse import urljoin
 
 
 class Proxy(DjangoHandlerMixin, RequestHandler):
-
     async def post(self, relative_url):
-        if not hasattr(settings, 'PHPLIST_BASE_URL'):
+        if not hasattr(settings, "PHPLIST_BASE_URL"):
             self.finish()
         self.url = urljoin(
-            settings.PHPLIST_BASE_URL,
-            '/admin/?page=call&pi=restapi'
+            settings.PHPLIST_BASE_URL, "/admin/?page=call&pi=restapi"
         )
         self.relative_url = relative_url
         await self.login()
 
     async def login(self):
         post_data = {
-            'cmd': 'login',
-            'login': settings.PHPLIST_LOGIN,
-            'password': settings.PHPLIST_PASSWORD
+            "cmd": "login",
+            "login": settings.PHPLIST_LOGIN,
+            "password": settings.PHPLIST_PASSWORD,
         }
-        if hasattr(settings, 'PHPLIST_SECRET'):
-            post_data['secret'] = settings.PHPLIST_SECRET
+        if hasattr(settings, "PHPLIST_SECRET"):
+            post_data["secret"] = settings.PHPLIST_SECRET
         http = AsyncHTTPClient()
         try:
             response = await http.fetch(
-                HTTPRequest(
-                    self.url,
-                    'POST',
-                    None,
-                    urlencode(post_data)
-                )
+                HTTPRequest(self.url, "POST", None, urlencode(post_data))
             )
         except ConnectionRefusedError:
             self.set_status(404)
@@ -46,8 +39,8 @@ class Proxy(DjangoHandlerMixin, RequestHandler):
             self.set_status(404)
             self.finish()
             return
-        self.session_cookie = response.headers['Set-Cookie']
-        if self.relative_url == 'subscribe_email':
+        self.session_cookie = response.headers["Set-Cookie"]
+        if self.relative_url == "subscribe_email":
             await self.subscribe_email()
         else:
             self.set_status(401)
@@ -55,52 +48,52 @@ class Proxy(DjangoHandlerMixin, RequestHandler):
             return
 
     async def subscribe_email(self):
-        email = self.get_argument('email')
+        email = self.get_argument("email")
         email_object = EmailAddress.objects.filter(email=email).first()
         if not email_object:
             self.finish()
             return
         post_data = {
-            'cmd': 'subscriberAdd',
-            'email': email,
-            'foreignkey': '',
-            'confirmed': 1,
-            'htmlemail': 1,
-            'disabled': 0
+            "cmd": "subscriberAdd",
+            "email": email,
+            "foreignkey": "",
+            "confirmed": 1,
+            "htmlemail": 1,
+            "disabled": 0,
         }
-        if hasattr(settings, 'PHPLIST_SECRET'):
-            post_data['secret'] = settings.PHPLIST_SECRET
+        if hasattr(settings, "PHPLIST_SECRET"):
+            post_data["secret"] = settings.PHPLIST_SECRET
         http = AsyncHTTPClient()
         response = await http.fetch(
             HTTPRequest(
                 self.url,
-                'POST',
-                {'Cookie': self.session_cookie},
-                urlencode(post_data)
+                "POST",
+                {"Cookie": self.session_cookie},
+                urlencode(post_data),
             )
         )
         if response.error:
             response.rethrow()
         response_json = json_decode(response.body)
-        if response_json['status'] == 'error':
+        if response_json["status"] == "error":
             # could not create user with this email
             self.finish()
             return
-        subscriber_id = response_json['data']['id']
+        subscriber_id = response_json["data"]["id"]
         post_data = {
-            'cmd': 'listSubscriberAdd',
-            'list_id': settings.PHPLIST_LIST_ID,
-            'subscriber_id': subscriber_id
+            "cmd": "listSubscriberAdd",
+            "list_id": settings.PHPLIST_LIST_ID,
+            "subscriber_id": subscriber_id,
         }
-        if hasattr(settings, 'PHPLIST_SECRET'):
-            post_data['secret'] = settings.PHPLIST_SECRET
+        if hasattr(settings, "PHPLIST_SECRET"):
+            post_data["secret"] = settings.PHPLIST_SECRET
         http = AsyncHTTPClient()
         response = await http.fetch(
             HTTPRequest(
                 self.url,
-                'POST',
-                {'Cookie': self.session_cookie},
-                urlencode(post_data)
+                "POST",
+                {"Cookie": self.session_cookie},
+                urlencode(post_data),
             )
         )
         if response.error:
